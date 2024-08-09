b25d76f5389734fd9324417f423674c081afe098
Matt Hwang <mwhwang182@gmail.com>
2024-08-09 15:43:35 -0400

    implement gallery page

---
+++ src/app/gallery/Gallery.tsx
@@ -0,0 +1,79 @@
+"use client"
+import { RowsPhotoAlbum } from "react-photo-album"
+import "react-photo-album/rows.css";
+
+const Gallery = () => {
+
+    const photos = [
+        {
+            src: "/GalleryImage1.jpg",
+            width: 600,
+            height: 400
+        },
+        {
+            src: "/GalleryImage2.jpg",
+            width: 300,
+            height: 400
+        },
+        {
+            src: "/GalleryImage3.jpg",
+            width: 550,
+            height: 400
+        },
+        {
+            src: "/GalleryImage4.jpg",
+            width: 370,
+            height: 400
+        },
+        {
+            src: "/headshot.png",
+            width: 400,
+            height: 400
+        },
+        {
+            src: "/GalleryImage5.jpg",
+            width: 560,
+            height: 400
+        },
+        {
+            src: "/GalleryImage6.jpg",
+            width: 550,
+            height: 400
+        },
+        {
+            src: "/GalleryImage7.jpg",
+            width: 270,
+            height: 400
+        }
+    ]
+
+
+    const RenderImage = (props: any) => {
+        const { style, ...rest } = props;
+        return (
+            <div
+                style={{
+                    ...style,
+                    overflow: "hidden",
+                    borderRadius: "5px",
+                }}
+                {...rest}
+            >
+            </div>
+        )
+    }
+    return (
+
+        <RowsPhotoAlbum
+            photos={photos}
+            render={{
+                wrapper: RenderImage
+            }}
+            spacing={15}
+            rowConstraints={{ maxPhotos: 3 }}
+            targetRowHeight={300}
+        />
+    )
+}
+
+export default Gallery
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,17 @@
-const GalleryPage = () => {
-    return (
-        <div>
+import "react-photo-album/rows.css";
+import Gallery from "./Gallery";
 
+const GalleryPage = async () => {
+
+    return (
+        <div className="
+            size-full min-h-screen
+            pattern-isometric pattern-gray-200 pattern-bg-white pattern-size-10 pattern-opacity-100
+            pt-20
+        ">
+            <div className="max-w-[1000px] mx-auto px-8">
+                <Gallery />
+            </div>
         </div>
     )
 }
