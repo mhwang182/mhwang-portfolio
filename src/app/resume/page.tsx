c8f2493382091c0d759584fcf4cdb4493deb75df
Matt Hwang <mwhwang182@gmail.com>
2024-09-03 11:56:09 -0400

    implement resume download

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,3 +1,4 @@
+import CommonButton from "../Components/CommonButton"
 
 const ResumePage = () => {
     return (
@@ -6,10 +7,13 @@ const ResumePage = () => {
             pattern-isometric pattern-gray-200 pattern-bg-white pattern-size-10 pattern-opacity-100 
             px-4">
             <div className="
-                w-full max-w-[1000px] mx-auto pt-32
+                w-full max-w-[1000px] mx-auto pt-28
                 sm:aspect-[8/10] h-lvh sm:h-auto
             ">
-                <iframe src={"/resume.pdf"} width={"100%"} height={"100%"} />
+                <div className="mb-4">
+                    <a href="/Matthew Hwang Resume.pdf" download><CommonButton small text="Download Resume!" /></a>
+                </div>
+                <iframe src={"/Matthew Hwang Resume.pdf"} width={"100%"} height={"100%"} />
             </div>
         </div>
     )
